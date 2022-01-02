08c3cf0756dd3407c80d35ee8c48508985157771
Sunil <ksunilssah@gmail.com>
2022-01-02 11:33:17 +0530

    updated build

--- tiv-app/src/service/constants.js
+++ tiv-app/src/service/constants.js
@@ -1,5 +1,5 @@
 //export const apiURL = 'https://localhost:3000/';
-export const apiURL = 'https://tiv.arshendemo.xyz/api/';
+export const apiURL = 'https://decode.tradinginveins.com/api/';
 export const tradingViewURl = 'https://www.tradingview.com';
 export const highMomentum = 'high_momentum';
 export const volumeShocker = 'volume_shocker';
