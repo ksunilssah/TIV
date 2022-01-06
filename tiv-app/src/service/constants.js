6da0d05f4a4cf661a0edf363df359fda1a927d47
Sunil <ksunilssah@gmail.com>
2022-01-06 17:36:59 +0530

    udpated build folder

--- tiv-app/src/service/constants.js
+++ tiv-app/src/service/constants.js
@@ -1,12 +1,12 @@
-export const apiURL = 'https://localhost:3000/';
-//export const apiURL = 'https://decode.tradinginveins.com/api/';
+//export const apiURL = 'https://localhost:3000/';
+export const apiURL = 'https://decode.tradinginveins.com/api/';
 export const tradingViewURl = 'https://www.tradingview.com';
 export const highMomentum = 'high_momentum';
 export const volumeShocker = 'volume_shocker';
 export const sectorView = 'sectoral_view';
 export const indexView = 'index_view';
 export const marketStatus = 'market_status';
-export const callFrequency = 6000;
+export const callFrequency = 60000;
 
 export const defaultColDef = {
 	sortable: true,
